855ea30ff2d16f05456287547ebd400deba94652
Alexander Ursu <alexander.ursu@gmail.com>
2023-09-22 10:07:50 +0200

    Added comment with URL to kubernetes doc

--- encryption-config-file.sh
+++ encryption-config-file.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://kubernetes.io/docs/tasks/administer-cluster/encrypt-data/#write-an-encryption-configuration-file
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
 cat > encryption-config.yaml <<EOF
