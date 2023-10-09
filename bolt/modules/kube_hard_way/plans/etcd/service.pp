c79ffaf37a0b2bb8644003b16d60c5283f006d72
Alexander Ursu <alexander.ursu@gmail.com>
2023-10-09 17:10:24 +0200

    Added plan to set up and run ETCD service

---
+++ bolt/modules/kube_hard_way/plans/etcd/service.pp
@@ -0,0 +1,20 @@
+plan kube_hard_way::etcd::service (
+  TargetSpec $targets = 'controllers',
+  Stdlib::Unixpath $server_crt = '/etc/kubernetes/pki/kubernetes.pem',
+  Stdlib::Unixpath $server_key = '/etc/kubernetes/pki/kubernetes-key.pem',
+  Stdlib::Unixpath $peer_crt = '/etc/kubernetes/pki/kubernetes.pem',
+  Stdlib::Unixpath $peer_key = '/etc/kubernetes/pki/kubernetes-key.pem',
+  Stdlib::Unixpath $ca_crt = '/etc/kubernetes/pki/ca.pem',
+  Optional[String] $initial_cluster_token = 'etcd-cluster-0',
+  Optional[String] $initial_cluster = undef,
+) {
+  run_plan('etcd::service', $targets,
+    server_crt => $server_crt, server_key => $server_key,
+    peer_crt => $peer_crt, peer_key => $peer_key,
+    ca_crt => $ca_crt,
+  initial_cluster_token => $initial_cluster_token)
+
+  apply($targets) {
+    include etcd::service
+  }
+}
