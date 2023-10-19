2a24feb944c0f06c2adb29e65120a59665631171
Alexander Ursu <alexander.ursu@gmail.com>
2023-10-19 02:08:43 +0200

    Copy admin config into default location

--- bolt/plans/bootstrap/components.pp
+++ bolt/plans/bootstrap/components.pp
@@ -2,9 +2,16 @@ plan kubernetes::bootstrap::components (
   TargetSpec $main_controller = 'controller-0',
 ) {
   run_plan(facts, $main_controller)
+  $cert_dir = '/etc/kubernetes/pki'
+  $admin_config = "${cert_dir}/admin.kubeconfig"
+
   apply($main_controller) {
     class { 'kube_hard_way::bootstrap::coredns':
-      kubeconfig => '/etc/kubernetes/pki/admin.kubeconfig',
+      kubeconfig => $admin_config,
+    }
+
+    class { 'kubeinstall::kubectl::config':
+      kubeconfig => $admin_config,
     }
   }
 }
