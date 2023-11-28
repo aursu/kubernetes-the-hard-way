3664a86723027d00c2ba16a54e47dcca8e86143a
Alexander Ursu <alexander.ursu@gmail.com>
2023-11-28 15:23:50 +0100

    Fixed coredns version to globlas class

--- bolt/modules/kube_hard_way/manifests/bootstrap/coredns.pp
+++ bolt/modules/kube_hard_way/manifests/bootstrap/coredns.pp
@@ -9,10 +9,12 @@
 #   include kube_hard_way::bootstrap::coredns
 class kube_hard_way::bootstrap::coredns (
   Stdlib::Unixpath $kubeconfig = '/etc/kubernetes/admin.conf',
-  Stdlib::IP::Address $dns_addr = $kube_hard_way::params::dns_addr,
-) inherits kube_hard_way::params {
+  Stdlib::IP::Address $dns_addr = $kube_hard_way::global::dns_addr,
+) inherits kube_hard_way::global {
   include kubeinstall
   include kubeinstall::install::helm_binary
+
+  $coredns_version = $kube_hard_way::global::coredns_version
 
   kubeinstall::helm::repo { 'coredns':
     url => 'https://coredns.github.io/helm',
@@ -20,6 +22,7 @@ class kube_hard_way::bootstrap::coredns (
 
   kubeinstall::helm::chart { 'coredns/coredns':
     release_name      => 'coredns',
+    chart_version     => $coredns_version,
     namespace         => 'kube-system',
     default_namespace => true,
     kubeconfig        => $kubeconfig,
