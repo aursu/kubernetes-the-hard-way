f28ea1ddd7f413f8699eb9170f2c8627f492fd0b
Alexander Ursu <alexander.ursu@gmail.com>
2023-11-29 16:50:17 +0100

    Removed pod_cidr metadata setting in favor of Calico CNI

--- bolt/plans/bootstrap/worker.pp
+++ bolt/plans/bootstrap/worker.pp
@@ -4,20 +4,8 @@ plan kubernetes::bootstrap::worker (
   get_targets($targets).each |$target| {
     run_plan(facts, $target)
 
-    $pod_cidr_result = run_task('kube_hard_way::pod_cidr', $target)
-
-    # check if pod_cidr has been got
-    if $pod_cidr_result.ok {
-      $pod_cidr = $pod_cidr_result.first.value['pod_cidr']
-    }
-    else {
-      fail_plan('Unable ot retrieve Pod CIDR from instance metadata')
-    }
-
     apply($target) {
-      class { 'kube_hard_way::bootstrap::worker':
-        pod_subnet => $pod_cidr,
-      }
+      class { 'kube_hard_way::bootstrap::worker': }
     }
   }
 }
--- bootstap/gcloud-kubernetes-workers.sh
+++ bootstap/gcloud-kubernetes-workers.sh
@@ -8,10 +8,8 @@ for i in 0 1 2; do
     --image-family ubuntu-2204-lts \
     --image-project ubuntu-os-cloud \
     --machine-type e2-standard-2 \
-    --metadata pod-cidr=10.200.${i}.0/24 \
     --private-network-ip 10.240.0.2${i} \
     --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
     --subnet kubernetes \
     --tags kubernetes-the-hard-way,worker
 done
-
