544b04ccfc7048bc06293fd293844a7dcaf90b42
Alexander Ursu <alexander.ursu@gmail.com>
2023-12-03 23:03:47 +0100

    Added bootstrap script for GCE k8s controller build

---
+++ bootstrap/gcloud-gce.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+[ -d ingress-gce ] || { echo "Git submodule ingress-gce not found in $(pwd)/ingress-gce."; exit -1; }
+
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+(cd .. && git submodule update --init && git submodule sync)
+
+export VERSION_TAG=v1.26.0
+
+(cd ingress-gce && git checkout ${VERSION_TAG} -b ${VERSION_TAG})
+
+docker-compose build  --build-arg path=$(pwd) rocky8docker
+
+# docker run --rm --sig-proxy=true -u $(id -u):$(id -g) -v $(pwd)/.go:/go \
+#     -v $(pwd):/go/src/k8s.io/ingress-gce \
+#     -v $(pwd)/bin/amd64:/go/bin/linux_amd64 \
+#     -v $(pwd)/.go/std/amd64:/usr/local/go/pkg/linux_amd64_static \
+#     -v $(pwd)/.go/cache:/.cache/go-build \
+#     -w /go/src/k8s.io/ingress-gce golang:1.20.10 \
+#     /bin/sh -c "ARCH=amd64 OS=linux VERSION=v1.26.0 PKG=k8s.io/ingress-gce \
+#         TARGET=bin/amd64/glbc GIT_COMMIT=bdbf2d0c054f49cc78e299b45c01b28281d64e85 ./build/build.sh"
