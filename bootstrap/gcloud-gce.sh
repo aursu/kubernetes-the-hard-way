90f4491ef51f4b7b215f293d7daa66b9988e760f
Alexander Ursu <alexander.ursu@gmail.com>
2023-12-04 01:44:16 +0100

    Added run command

--- bootstrap/gcloud-gce.sh
+++ bootstrap/gcloud-gce.sh
@@ -13,11 +13,5 @@ git clone https://github.com/aursu/ingress-gce.git
 
 docker-compose build --build-arg path=$(pwd) rocky8docker
 
-# docker run --rm --sig-proxy=true -u $(id -u):$(id -g) -v $(pwd)/.go:/go \
-#     -v $(pwd):/go/src/k8s.io/ingress-gce \
-#     -v $(pwd)/bin/amd64:/go/bin/linux_amd64 \
-#     -v $(pwd)/.go/std/amd64:/usr/local/go/pkg/linux_amd64_static \
-#     -v $(pwd)/.go/cache:/.cache/go-build \
-#     -w /go/src/k8s.io/ingress-gce golang:1.20.10 \
-#     /bin/sh -c "ARCH=amd64 OS=linux VERSION=v1.26.0 PKG=k8s.io/ingress-gce \
-#         TARGET=bin/amd64/glbc GIT_COMMIT=bdbf2d0c054f49cc78e299b45c01b28281d64e85 ./build/build.sh"
+docker-compose run -ti --rm -v $(pwd)/ingress-gce/bin/amd64:$(pwd)/ingress-gce/bin/amd64 \
+    -v ~/.docker/config.json:/root/.docker/config.json rocky8docker
